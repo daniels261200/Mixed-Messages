944b0a60ba3fafdebcddabb82a606f96c01e7ebf
DSamaniego <59575743+daniels261200@users.noreply.github.com>
2022-02-09 21:22:32 -0700

    Functions and Variables declared

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+//Program that returns fake random quotes
 
+//Arrays from which the quotes will be formed
+let bandArr=[];
+let songArr=[];
+let quoteArr=[];
+
+
+
+//Function to generate a random Index based on the given array length
+let randIndexGenerator(arr){
+    let randIndex = Math.floor(Math.random*arr.lenght)
+    return randIndex
+}
+
+//Link each random Index for easy calling
+let bandIndex = randIndexGenerator(bandArr);
+let songIndex = randIndexGenerator(songArr);
+let quoteIndex = randIndexGenerator(quoteArr)
